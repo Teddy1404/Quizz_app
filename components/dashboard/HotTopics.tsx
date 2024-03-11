c4de3dde9e806452ac130ace76f624f1fdd6315b
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-03-11 18:03:22 +0530

    database created

--- components/dashboard/HotTopics.tsx
+++ components/dashboard/HotTopics.tsx
@@ -29,7 +29,6 @@ const HotTopicsCard = async (props: Props) => {
       </CardHeader>
       <CardContent className="pl-2">
         {/* <WordCloud formattedTopics={formattedTopics} /> */}
-        
        
       </CardContent>
     </Card>
